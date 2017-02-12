1b93d551fff6bbca2a1044e4af16ed916c972d7d
javilobo8 <info.lobobot@gmail.com>
2017-02-12 14:58:32 +0100

    add more filters

--- LOLStats Monitor/UsersViewController.swift
+++ LOLStats Monitor/UsersViewController.swift
@@ -11,43 +11,163 @@ import Charts
 import Foundation
 import SwiftyJSON
 
-class UsersViewController: UIViewController {
-    
-    let urlPath = "http://lobobot.com:1337/users"
-    let auth = "211a6e6a1fc289045b9cf64839beefce"
+let blueColor = UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)
 
+class UsersViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let auth = "211a6e6a1fc289045b9cf64839beefce"
+    
     @IBOutlet weak var barChartView: BarChartView!
-    @IBOutlet weak var refreshButton: UIBarButtonItem!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var usersTable: UITableView!
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var selectedDate: String!
+    var pickOptions: [String]!
+    
+    var pickDays: [String]!
+    var pickMonths: [String]!
+    var pickYears: [String]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        barChartView.delegate = self
+        
+        pickDays = self.generateDays()
+        pickMonths = self.generateMonths()
+        pickYears = self.generateYears()
+        
+        pickOptions = pickDays
+
+        pickerView.delegate = self
+        pickerView.selectRow(pickOptions.count - 1, inComponent: 0, animated: false)
+        
+        let initialDate = pickOptions[pickOptions.count - 1]
+        selectedDate = initialDate
+        
         barChartView.pinchZoomEnabled = false
         barChartView.drawGridBackgroundEnabled = false
-        barChartView.chartDescription?.enabled = false
+//        barChartView.chartDescription?.enabled = false
         barChartView.dragEnabled = false
         barChartView.leftAxis.axisMinimum = 0
         barChartView.leftAxis.drawBottomYLabelEntryEnabled = false
         barChartView.leftAxis.drawTopYLabelEntryEnabled = false
+        barChartView.leftAxis.enabled = true
         barChartView.rightAxis.enabled = false
         barChartView.xAxis.drawGridLinesEnabled = false
         barChartView.xAxis.labelPosition = XAxis.LabelPosition.bottom
         barChartView.legend.enabled = false
         barChartView.fitBars = true
+        
+        let numberF = NumberFormatter()
+        numberF.minimumFractionDigits = 0
+        numberF.maximumFractionDigits = 1
+        numberF.negativeSuffix = " $";
+        numberF.positiveSuffix = " $";
+        
+//        barChartView.leftAxis.valueFormatter = IAxisValueFormatter(numberF)
+
         self.requestData()
     }
-
-    @IBAction func refreshActionButton(_ sender: Any) {
-        self.requestData()
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    func generateDays() -> [String] {
+        let calendar = Calendar.current
+        var startDate = DateComponents()
+        startDate.year = 2016
+        startDate.month = 7
+        startDate.day = 1
+        var date = calendar.date(from: startDate)!
+        let endDate = Date()
+        let fmt = DateFormatter()
+        fmt.dateFormat = "yyyy-MM-dd"
+        var dates:[String] = []
+        while date <= endDate {
+            dates.append(fmt.string(from: date))
+            date = calendar.date(byAdding: .day, value: 1, to: date)!
+        }
+        return dates
+    }
+    
+    func generateMonths() -> [String] {
+        let calendar = Calendar.current
+        var startDate = DateComponents()
+        startDate.year = 2016
+        startDate.month = 7
+        startDate.day = 1
+        var date = calendar.date(from: startDate)!
+        let endDate = Date()
+        let fmt = DateFormatter()
+        fmt.dateFormat = "yyyy-MM"
+        var dates:[String] = []
+        while date <= endDate {
+            dates.append(fmt.string(from: date))
+            date = calendar.date(byAdding: .month, value: 1, to: date)!
+        }
+        return dates
+    }
+    
+    func generateYears() -> [String] {
+        return ["2016", "2017"]
+    }
+    
+    func replaceSegments(segments: Array<String>) {
+//        for segment in segments {
+//            self.insertSegmentWithTitle(segment, atIndex: self.numberOfSegments, animated: false)
+//        }
+    }
+
+    @IBAction func changeDateInterval(_ sender: UISegmentedControl) {
+        let selected = sender.selectedSegmentIndex
+        switch selected {
+        case 0:
+            self.pickOptions = pickDays
+            break
+        case 1:
+            self.pickOptions = pickMonths
+            break
+        case 2:
+            self.pickOptions = pickYears
+            break
+        default:
+            self.pickOptions = pickDays
+            break
+        }
+        self.pickerView.reloadAllComponents()
+        self.pickerView.selectRow(pickOptions.count - 1, inComponent: 0, animated: true)
+        selectedDate = pickOptions.last
+        self.requestData()
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickOptions.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickOptions[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedDate = pickOptions[row]
+        self.requestData()
+    }
+    
     func requestData() {
+        var urlPath = "http://lobobot.com:1337/users"
+
+        if self.selectedDate != nil {
+            urlPath = urlPath + "?date=" + self.selectedDate
+//            print("\(urlPath)")
+        }
+
         guard let endpoint = URL(string: urlPath) else {
             print("Error creating endpoint")
             return
@@ -60,27 +180,15 @@ class UsersViewController: UIViewController {
         let task = session.dataTask(with: request) {(data, response, err) in
             let json = JSON(data: data!)
 
-            let totalUsers = json["total"].int32
-
-            let chartData = json["data"].array
+            let chartData = json.array
             var arrayData = Array<Double>()
             for (_, number):(String, JSON) in JSON(chartData!) { arrayData.append(number.double!) }
             
-            let newUsers = json["list"].array
-            var arrayUsers = Array<String>()
-            for (_, user):(String, JSON) in JSON(newUsers!) { arrayUsers.append(user.string!) }
-            
             DispatchQueue.main.async{
-                self.totalLabel.text = "Total users \(totalUsers!)"
                 self.printChart(data: arrayData)
-                self.printList(data: arrayUsers)
             }
         }
         task.resume()
-    }
-    
-    func printList(data: Array<String>) {
-        print("\(data)")
     }
     
     func printChart(data: Array<Double>) {
@@ -88,12 +196,13 @@ class UsersViewController: UIViewController {
         let data = BarChartData()
         
         let ds1 = BarChartDataSet(values: yse1, label: "Hello")
-        ds1.colors = [UIColor(red: 0, green: 122/255, blue: 1, alpha: 1)]
+        barChartView.chartDescription?.text = "Max: \(Int(ds1.yMax))"
+        ds1.colors = [blueColor]
         ds1.drawValuesEnabled = false
         data.addDataSet(ds1)
         
         self.barChartView.data = data
-        self.barChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0)
+        self.barChartView.animate(xAxisDuration: 0.5, yAxisDuration: 0.5)
         self.view.reloadInputViews()
     }
 }
